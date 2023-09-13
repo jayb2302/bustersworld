fdaf209eb4352d425dc016a73c8fb78a3a812ddd
jayb2302 <jonbja01@easv365.dk>
2023-09-13 14:21:20 +0200

    Actors list almost done

--- director-cast.php
+++ director-cast.php
@@ -21,7 +21,6 @@ $leave_review_button_text = get_field('leave_review_button_text');
 <div class="page-wrapper">
     
     <div class="directors-container">
-
         <div class="directors-info">
             <img class="directors-photo" src="<?php echo esc_url($directors_photo['url']); ?>" alt="<?php echo esc_attr($directors_name); ?>">
             <h2><?php echo esc_html($directors_name); ?></h2>
@@ -30,52 +29,66 @@ $leave_review_button_text = get_field('leave_review_button_text');
             </div>
         </div>
     </div>
-
-    <div class="actors-list">
-        <h2><?php echo esc_html(get_field('actors_list')); ?></h2>
-        <ul>
-            <?php 
-            
-            $actors_query = new WP_Query(array(
-                'post_type' => 'actor', // Custom post type
-                'posts_per_page' => -1,
-            ));
-           
-             if ($actors_query->have_posts()) :
-                while ($actors_query->have_posts()) : $actors_query->the_post();
-            ?>
-            <div class="actors-container">
-                    <img class="actor-photo" src="<?php echo esc_url(get_field('actor-photo')['url']); ?>" alt="<?php echo esc_attr(get_the_title()); ?>">
-                    <h4>
-                    <?php echo esc_html(get_field('actors_name')); ?>
-                    </h4>
-
-                    <!-- Display other custom fields -->
-                    <h3> <?php echo esc_html(get_field('character-name')); ?></h3>
-                    <p> <?php echo esc_html(wp_trim_words(get_field('biography'), 20)); ?></p>
-                    <p>Social Media Profile: <a href="<?php echo esc_url(get_field('social-media-profile')); ?>" target="_blank"><?php echo esc_html(get_field('social-media-profile')); ?></a></p>
-            </div>
-            <?php
-                endwhile;
-                wp_reset_postdata();
-            else :
-                echo 'No actors found.';
-            endif; 
-            
-            ?>
-           
-           
-        </ul>
-      
-    </div>
    
+    
 </div>
- <?php
-            $review_shortcode = get_post_meta(get_the_ID(), 'review_shortcode', true);
-            echo do_shortcode($review_shortcode);
+
+
+<div class="actors-wrapper">
+        <div class="actors-scroll-container">
+            <div class="horizontal-scroll">
+           
+                <div class="line"></div>
+            
+            
+                    <?php 
+                    
+                    $actors_query = new WP_Query(array(
+                        'post_type' => 'actor', // Custom post type
+                        'posts_per_page' => -1,
+                    ));
+                
+                    if ($actors_query->have_posts()) :
+                        while ($actors_query->have_posts()) : $actors_query->the_post();
+                    ?>
+                    <div class="actors-container">
+                        <div class="actor-item">
+                            <img class="actor-photo" src="<?php echo esc_url(get_field('actor-photo')['url']); ?>" alt="<?php echo esc_attr(get_the_title()); ?>">
+                            <h4>
+                            <?php echo esc_html(get_field('character-name')); ?>
+                            </h4>
+                            <h3> <?php echo esc_html(get_field('actors_name')); ?></h3>
+
+                            <p> <?php echo esc_html(wp_trim_words(get_field('biography'), 25)); ?></p>
+
+                            <a href="<?php echo esc_url(get_field('social-media-profile')); ?>" target="_blank"><?php echo esc_html(get_field('social-media-profile')); ?></a>
+                        </div>
+                    </div>
+                    <?php
+                        endwhile;
+                        wp_reset_postdata();
+                    else :
+                        echo 'No actors found.';
+                    endif; 
+                    
+                    ?>
+                
+                
+            
+            
+            </div>
+        </div>
+    </div>
+
+<div class="review-section">
+    <?php if (function_exists('the_ratings')) : ?>
+    <?php the_ratings(); ?>
+    <?php endif; ?>
+    <?php
+        $review_shortcode = get_post_meta(get_the_ID(), 'review_shortcode', true);
+        echo do_shortcode($review_shortcode);
     ?>
-
-
+</div>
 <!-- List of Sponsors -->
 <div class="sponsors-list">
     <?php if ($sponsors) : ?>
@@ -95,19 +108,11 @@ $leave_review_button_text = get_field('leave_review_button_text');
 </div>
 
 <!-- Review Section -->
-<?php if (function_exists('the_ratings')) : ?>
-    <div class="review-section">
-        <h2>User Reviews</h2>
-        <?php the_ratings(); ?>
-    </div>
-<?php endif; ?>
+
 
 <!-- Leave a Review Button -->
 <a href="#review-form" class="leave-review-button"><?php echo esc_html($leave_review_button_text); ?></a>
 
-<!-- Page Footer -->
-<footer>
-    <!-- Footer content goes here -->
-</footer>
+
 
 <?php get_footer(); ?>
